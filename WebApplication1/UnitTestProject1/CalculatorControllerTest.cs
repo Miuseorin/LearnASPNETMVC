52b57f17faaa6279416240956184c1b6b2a8fefa
Miuseorin <dacvutinhyeumanga@gmail.com>
2019-03-07 15:31:26 +0700

    Tao layout va thu test

---
+++ WebApplication1/UnitTestProject1/CalculatorControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestAuthor()
+        {
+            var controller = new CalculatorController();
+            var result = controller.ShowAuthor();
+
+            Assert.AreEqual("Nguyen Thi Ngoc Tran", result);
+        }
+    }
+}
