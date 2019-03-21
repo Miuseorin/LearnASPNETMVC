c59557ecf96002d5bfeb18703d6a73df02ee7b44
tuangd01 <tuangd01@gmail.com>
2019-03-21 15:15:04 +0700

    21032019

---
+++ Web21032019/Web21032019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web21032019.MvcApplication" Language="C#" %>
---
+++ Web21032019/Web21032019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web21032019.Startup))]
+namespace Web21032019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
