2aa874543d01db9956b6b7b2528931dbe294b771
Miuseorin <dacvutinhyeumanga@gmail.com>
2019-04-11 15:39:33 +0700

    11/04/2019

---
+++ Expenditure/Expenditure.Tests/Controllers/ExpenditureControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Expenditure;
+using Expenditure.Controllers;
+using Expenditure.Models;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace Expenditure.Tests.Controllers
+{
+    [TestClass]
+    public class ExpenditureControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new B();
+            var controller = new ExpenditureController();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<B>));
+            
+        }
+    }
+}
---
+++ Expenditure/Expenditure/Models/ExpenditureController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Expenditure.Models
+{
+    public class ExpenditureController : Controller
+    {
+        private ExpenditureEntities db = new ExpenditureEntities();
+
+        // GET: /Expenditure/
+        public ActionResult Index()
+        {
+            var model = db.B.ToList();
+            return View(model);
+        }
+
+        // GET: /Expenditure/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = db.B.Find(id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            B b = db.B.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // GET: /Expenditure/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Expenditure/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(B model)
+        {
+            ValidateExpenditure(model);
+            if (ModelState.IsValid)
+            {
+                model.Date = DateTime.Today;
+                db.B.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        private void ValidateExpenditure(B model)
+        {
+            if (model.Amount <= 0)
+                ModelState.AddModelError("Amount", "Số tiền quá ít!");
+        }
+        // GET: /Expenditure/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            B b = db.B.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+
+        // POST: /Expenditure/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Date,Amount,Note")] B b)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(b).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(b);
+        }
+
+        // GET: /Expenditure/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            B b = db.B.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+
+        // POST: /Expenditure/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            B b = db.B.Find(id);
+            db.B.Remove(b);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
